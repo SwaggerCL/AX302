1d8dafe2fe7cc754cd029db234b20d9767f0e07e
SwaggerCL <sakejittl@gmail.com>
2019-01-19 18:01:36 +0800

    project 3

--- temp/app.js
+++ temp/app.js
@@ -1,31 +1,27 @@
 console.log("Test...")
 var c =document.getElementById("myCanvas")
 var ctx= c.getContext("2d");
-ctx.moveTo(50,50);
-ctx.lineTo(250,250);
-ctx.moveTo(250,50);
-ctx.lineTo(50,250);
-ctx.strokeStyle = "rgo(50,255,50)";
-ctx.lineWidth = 5;
+ctx.arc(150,150,100,0,6.28)
+ctx.strokeStyle = "blue";
+ctx.stroke()
+ctx.fillStyle = "cyan";
+ctx.fill();
+ctx.beginPath();
+ctx.moveTo(150,20);
+ctx.lineTo(10,150);
+ctx.lineTo(290,150);
+ctx.closePath();
+ctx.strokeStyle = "green";
 ctx.stroke();
-ctx.fillStyle = "darkgreen";
-ctx.fillRect(100,100,100,100);
-ctx.strokeStyle = "red";
-ctx.strokeRect(50,50,200,200)
-ctx.clearRect(125,125,50,50);
-var c1 =document.getElementById("j")
+ctx.fillStyle = "lightgreen";
+ctx.fill();
+var c1 =document.getElementById("j");
 var ctx1= c1.getContext("2d");
-ctx1.moveTo(50,50);
-ctx1.lineTo(250,250);
-ctx1.moveTo(250,50);
-ctx1.lineTo(50,250);
-ctx1.strokeStyle = "rgb(150,255,205)";
-ctx1.lineWidth = 55;
-ctx.stroke();
-ctx.fillStyle = "pink";
-ctx.fillRect(100,100,100,100);
-ctx.strokeStyle = "grey";
-ctx.strokeRect(50,50,200,200)
-ctx.clearRect(125,125,50,50);
-ctx1.fillRect(150,150,150,150);
-ctx1.fillRect(0,0,150,150);
+ctx1.beginPath();
+ctx1.moveTo(0,0);
+ctx1.lineTo(300,300);
+ctx1.lineTo(300,0);
+ctx1.lineTo(0,300);
+ctx1.closePath();
+ctx1.strokeStyle = "red";
+ctx1.stroke();
