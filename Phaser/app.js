ad4807255b57a8ccd7280d67979941a1b75b4ac2
SwaggerCL <sakejittl@gmail.com>
2019-03-02 17:58:36 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -31,10 +31,47 @@ function create(){
 	liveslabel.setShadow(3, 3, "rgba(0,0,0,0,5)", 2);
 	liveslabel.setShadow(3, 3, "rgba(0,0,0,0,5)", 2);
 	player = game.add.sprite(10, 300, "dude");
-	player animations.add("left", [0,1,2,3], 10, true);
-	player animations.add("right", [5,6,7,8], 10, true);
+	player.animations.add("left", [0,1,2,3], 10, true);
+	player.animations.add("right", [5,6,7,8], 10, true);
+	game.physics.arcade.enable(player);
 	player.body.gravity.y = 300;
 	player.body.bounce.y = 0.2;
-	player.body.collide
+	player.body.collideWorldBounds = true;
+	enemy = game.add.sprite(20, 200, "baddie");
+		enemy.animations.add("left", [0,1], 10, true);
+		enemy.animations.add("right", [2,3], 10, true);
+		game.physics.arcade.enable(enemy);
+		enemy.body.gravity.y = 500;
+		enemy.body.bounce.y = 0.2;
+		enemy.body.collideWorldBounds = true;
+		stars = game.add.physicsGroup();
+		stars.enablebody = true;
+		for(var i = 0; i < 12; i++){
+var star = stars.create( i*70 , 0 , "star");
+star.body.gravity.y = 200;
+star.body.bounce.y = Math.random()*0.2 + 0.7
+		}
+		cursors = game.input.keyboard.createCursorKeys()
+		wKey = game.input.keyboard.addKey(Phaser.Keyboard.W)
+		aKey = game.input.keyboard.addKey(Phaser.Keyboard.A)
+		sKey = game.input.keyboard.addKey(Phaser.Keyboard.S)
+	}
+function update(){
+	game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.collide(stars, platforms);
+player.body.velocity.x = 0;
+if(cursors.left.isDown || aKey.isDown){
+	player.body.velocity.x = -150;
+	player.animations.play.("left");
+	} else if(cursors.right.isDown || dKey.isDown){
+	player.body.velocity.x = 150;
+	player.animations.play.("right");
+}else{
+	player.animations.stop();
+	player.frame = 4;
 }
-function update(){}
+if(cursors.up.isDown || wKey.isDown && player.body.touching.down){
+	player.body.velocity.y = -300;
+}
+}
