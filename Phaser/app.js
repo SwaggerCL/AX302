c04260ba69c2b2164d1b9cadfc2023d2cac3a686
SwaggerCL <sakejittl@gmail.com>
2019-03-02 18:03:44 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -64,10 +64,10 @@ function update(){
 player.body.velocity.x = 0;
 if(cursors.left.isDown || aKey.isDown){
 	player.body.velocity.x = -150;
-	player.animations.play.("left");
+	player.animations.play("left");
 	} else if(cursors.right.isDown || dKey.isDown){
 	player.body.velocity.x = 150;
-	player.animations.play.("right");
+	player.animations.play("right");
 }else{
 	player.animations.stop();
 	player.frame = 4;
